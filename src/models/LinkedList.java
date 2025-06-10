c77f11032e0d25eb1a482603aeff35dd28bcbe9e
Erika-colla <collaguazobelen8@gmail.com>
2025-06-10 08:49:07 -0500

    feat: Add metods at LinkedList

--- src/models/LinkedList.java
+++ src/models/LinkedList.java
@@ -10,15 +10,49 @@ public class LinkedList<T> {
     }
 
     public void appendToTail(T value){
-
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext(); 
+            }
+            current.setNext(newNode); 
+        }
+        size++;
     }
 
-    public void findByValue(T value ){
-
+    public void findByValue(T value){
+        Node<T> current = head;
+        while (current != null) {
+            if (current.getValue().equals(value)) {
+                System.out.println("Elemento encontrado: " + value);
+                return;
+            }
+            current = current.getNext();
+        }
+        System.out.println("Elemento no encontrado: " + value);
     }
 
     public void deleteByValue(T value){
-
+        if (head == null) return; 
+    
+        if (head.getValue().equals(value)) {
+            head = head.getNext(); 
+            size--;
+            return;
+        }
+    
+        Node<T> current = head;
+        while (current.getNext() != null) {
+            if (current.getNext().getValue().equals(value)) {
+                current.setNext(current.getNext().getNext()); 
+                size--;
+                return;
+            }
+            current = current.getNext();
+        }
     }
 
     public int size() {
