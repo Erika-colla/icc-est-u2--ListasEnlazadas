d74131d68639ff9770452d6e0966be27d408c57d
Bryan <bryanbarrosvilla@gmail.com>
2025-06-11 08:55:55 -0500

    Add: App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import controllers.ContactManager;
+import controllers.MenuController;
+import views.ConsoleView;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        ConsoleView consoleView = new ConsoleView();
+        ContactManager contactManager = new ContactManager();
+        MenuController menuController = new MenuController(contactManager, consoleView);
+
+        menuController.showMenu();
+        consoleView.closeScanner();
     }
 }
